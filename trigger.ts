f3af2f1d1809ac8ebd814d4a64262f4dfe105afa
mayank-rf <mayank.vats@rocketfrog.ai>
2025-01-13 20:01:14 +0530

    trigger updates

--- trigger.ts
+++ trigger.ts
@@ -1,7 +1,7 @@
 import { TriggerClient } from "@trigger.dev/sdk";
 
 export const client = new TriggerClient({
-  id: "papermark-dev-Ojwu",
+  id: "papermark-rf",
   apiKey: process.env.TRIGGER_API_KEY,
   apiUrl: process.env.TRIGGER_API_URL,
 });
